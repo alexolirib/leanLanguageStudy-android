a8bade26464a4210f7bdf9d78828c3a070e230d7
alexolirib <ribeirolx17@gmail.com>
2023-11-09 20:33:13 -0300

    create many activities (colors, family, numbers, phrases)

--- app/src/main/java/com/example/android/leanLanguageStudy/MainActivity.java
+++ app/src/main/java/com/example/android/leanLanguageStudy/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.android.leanLanguageStudy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 // import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +17,48 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        numbers.setOnClickListener(view -> {
+            Intent i = new Intent(this, NumbersActivity.class);
+            startActivity(i);
+        });
+        TextView familyMenbers = (TextView) findViewById(R.id.family);
+        familyMenbers.setOnClickListener(view -> {
+            Intent i = new Intent(this, FamilyActivity.class);
+            startActivity(i);
+        });
+        TextView colors = (TextView) findViewById(R.id.colors);
+        colors.setOnClickListener(view -> {
+            Intent i = new Intent(this, ColorsActivity.class);
+            startActivity(i);
+        });
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+        phrases.setOnClickListener(view -> {
+            Intent i = new Intent(this, PhrasesActivity.class);
+            startActivity(i);
+        });
+
+
+
     }
+
+//    public void openNumbersList(View view) {
+//        Intent i = new Intent(this, NumbersActivity.class);
+//        startActivity(i);
+//    }
+//
+//    public void openColorsList(View view) {
+//        Intent i = new Intent(this, ColorsActivity.class);
+//        startActivity(i);
+//    }
+//
+//    public void openFamilyList(View view) {
+//        Intent i = new Intent(this, FamilyActivity.class);
+//        startActivity(i);
+//    }
+//
+//    public void openPhrasesList(View view) {
+//        Intent i = new Intent(this, PhrasesActivity.class);
+//        startActivity(i);
+//    }
 }
---
+++ app/src/main/java/com/example/android/leanLanguageStudy/NumbersClickListener.java
@@ -0,0 +1,15 @@
+package com.example.android.leanLanguageStudy;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
+import android.view.View;
+
+public class NumbersClickListener implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+//        Intent i = new Intent(null, NumbersActivity.class);
+//        startActivity(i);
+//        startActivity(i);
+    }
+}
